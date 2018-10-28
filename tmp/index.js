d4dc89dd0e5a8131527dff07247d8157f1e79f3a
艾伦 <529360897@qq.com>
2018-10-28 20:58:10 +0800

    add tmp files

---
+++ tmp/index.js
@@ -0,0 +1,8 @@
+const models = [
+	require('./../login.js'),
+	require('./../route.js'),
+	require('./../subject.js'),
+	require('./../user.js'),
+];
+module.exports = models;
+module.exports.default = models;
